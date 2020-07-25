c954ffc9099a10210429cc2ee269e2a3358890ea
hyepark93 <hyepark93@gamil.com>
2020-07-25 19:11:01 +0900

    hhhhh

--- ShoppingWeb/src/main/java/com/project/shoppingweb/controller/IndexController.java
+++ ShoppingWeb/src/main/java/com/project/shoppingweb/controller/IndexController.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -193,11 +192,11 @@ public class IndexController {
 		return "mypage";
 	}	
 	
-	 @RequestMapping("list")
+	@RequestMapping("memInfo")
 	    public String listAll(Model model) {
 	        List<shopDTO> list = shopService.listAll();
 	       model.addAttribute("list",list);
 	        return "member_list";
-	    }
+	}
 	
 }
